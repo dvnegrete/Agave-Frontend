647fc8aa0495ca8eb9886614ff9a99aad1ecf895
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2025-10-10 12:09:28 -0600

    feat: Cconfigurar basename en App.tsx para deploy exitoso en Pages

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Agave-Frontend">
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/login" element={<Layout><Login /></Layout>} />
